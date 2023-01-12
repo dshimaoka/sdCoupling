095dde433b3761dd6dfdefe5f671aa7272399943
Daisuke <sageagainstthemachine@gmail.com>
2023-01-13 00:26:44 +1100

    mod to sbatch
    --array=1-81

--- script_wrapper.sh
+++ script_wrapper.sh
@@ -3,9 +3,10 @@
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-user=daisuke.shimaoka@monash.edu
-#SBATCH --job-name=Wrapper
-#SBATCH --time=20:00:00
-#SBATCH --ntasks=1-81
+#SBATCH --job-name=Wrapper_array
+#SBATCH --time=40:00:00
+#SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=33000
+#SBATCH --array=1-81
 module load matlab
 matlab -nodisplay -nodesktop -nosplash < wrapper_array.m
