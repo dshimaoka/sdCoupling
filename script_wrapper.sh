bbc4493a9be5f409f48eb477fb136edc5b36d6a2
Daisuke <sageagainstthemachine@gmail.com>
2023-01-13 00:19:20 +1100

    batch processing 81param space

--- script_wrapper.sh
+++ script_wrapper.sh
@@ -5,7 +5,7 @@
 #SBATCH --mail-user=daisuke.shimaoka@monash.edu
 #SBATCH --job-name=Wrapper
 #SBATCH --time=20:00:00
-#SBATCH --ntasks=1
+#SBATCH --ntasks=1-81
 #SBATCH --mem-per-cpu=33000
 module load matlab
-matlab -nodisplay -nodesktop -nosplash < wrapper.m
+matlab -nodisplay -nodesktop -nosplash < wrapper_array.m
