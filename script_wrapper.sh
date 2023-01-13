f5d6eaf6d204bce420c2ba1a6c51409029b8af50
Daisuke <sageagainstthemachine@gmail.com>
2023-01-14 03:07:22 +1100

    modify script_wrapper
    40 jobs

--- script_wrapper.sh
+++ script_wrapper.sh
@@ -7,6 +7,6 @@
 #SBATCH --time=40:00:00
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=33000
-#SBATCH --array=1-81
+#SBATCH --array=1-40
 module load matlab
 matlab -nodisplay -nodesktop -nosplash < wrapper_array.m
