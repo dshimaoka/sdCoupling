1b8c3e63f625daacc3618e494f6da6c2566e6b06
Daisuke <sageagainstthemachine@gmail.com>
2023-01-14 09:27:16 +1100

    3rd batch processing 4jobs
    external perturbation

---
+++ script_wrapper_input.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-user=daisuke.shimaoka@monash.edu
+#SBATCH --job-name=Wrapper_array
+#SBATCH --time=40:00:00
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=33000
+#SBATCH --array=1-4
+module load matlab
+matlab -nodisplay -nodesktop -nosplash < wrapper_input_array.m
--- wrapper_input_array.m
+++ wrapper_input_array.m
@@ -20,14 +20,14 @@ end
 
 
 %soma-dendrite coupling
-plrPer = [10];%5
+plrPer = [0 10];%5
 gsdPer = [15];%15
 
 %synaptic conductance
 gEEPer = [13];%15%cFac * gEEPer/100 = 1
 gIIPer = [20]; %[15 20 25 30];% 20 for SW?
 gEIPer = [20];%[100];% 15 20 30 40 50]; %10< for E-I balance
-gIEPer = [50];% 30 50 70 90];%[22];
+gIEPer = [20 50];% 30 50 70 90];%[22];
 
 sz = [numel(plrPer) numel(gsdPer) numel(gEEPer) numel(gIIPer) numel(gEIPer) numel(gIEPer)];
 %total jobs: prod(sz)
