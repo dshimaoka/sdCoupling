89bb48fc639dfca6df0bf93167d69957e2bd468a
Daisuke <sageagainstthemachine@gmail.com>
2023-01-19 00:20:28 +1100

    ntask=2

--- script_wrapper.sh
+++ script_wrapper.sh
@@ -5,7 +5,7 @@
 #SBATCH --mail-user=daisuke.shimaoka@monash.edu
 #SBATCH --job-name=Wrapper_array
 #SBATCH --time=40:00:00
-#SBATCH --ntasks=1
+#SBATCH --ntasks=2
 #SBATCH --mem-per-cpu=70000
 #SBATCH --array=1-4
 module load matlab
